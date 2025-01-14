b4cd11dea2be5ccc3eecdb2f2eacff6f1834380b
L1F21BSSE0580 <L1F21BSSE0580@ucp.com>
2025-01-14 16:24:59 +0500

    first commit

---
+++ f1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // Output "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
---
+++ f1.py
@@ -0,0 +1,2 @@
+# Output "Hello, World!" to the console
+print("Hello, World!")
